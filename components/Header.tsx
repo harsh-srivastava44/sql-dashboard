59f76e36088a77c408c2831c74263d238c4ffd95
harsh srivastava <harshsrivastava.dev@gmail.com>
2022-10-31 00:57:16 +0530

    changes to header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-
+import Image from 'next/image'
+import logo from 'public/atlan-blue.svg'
 export const Header = () => {
   return (
     <div className="">
       <header>
         <nav className="bg-gray-200    border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <ul className="grid grid-cols-3">
-            <li className="col-span-1">one</li>
-            <li className="col-span-1">two</li>
-            <li className="col-span-1">three</li>
+            <li className="col-span-1"><Image src={logo} alt="atlan" width="70" height="70" /></li> 
           </ul>
         </nav>
       </header>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Header } from "components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className="h-100 px-4 md:mx-auto aspect-ratio: auto;">
-    <Component {...pageProps} />
-  </div>
+  return (
+    <>
+      <Header/>
+      <div className="h-100 px-4 md:mx-auto aspect-ratio: auto;">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
